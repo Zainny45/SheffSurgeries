90de4523acf36057410ef6a45df94315d3fe6176
Zainny45 <b7015095@my.shu.ac.uk>
2019-04-07 19:58:19 +0100

    Created a functional search bar to search prescriptions in the doctors login

--- appointmentSystem/grails-app/controllers/com/zislam/PrescriptionController.groovy
+++ appointmentSystem/grails-app/controllers/com/zislam/PrescriptionController.groovy
@@ -3,4 +3,24 @@ package com.zislam
 class PrescriptionController {
 
     def scaffold= Prescription
+def advSearch(){
+}
+
+def advResults() {
+
+def prescriptionProps = Prescription.metaClass.properties*.name
+def prescriptions = Prescription.withCriteria {
+ "${params.queryType}" {
+
+
+params.each { field, value ->
+ if (prescriptionProps.grep(field) && value) {
+ ilike(field, value)
+ }
+ }
+ }
+ }
+
+ return [ prescriptions : prescriptions]
+ }
 }
