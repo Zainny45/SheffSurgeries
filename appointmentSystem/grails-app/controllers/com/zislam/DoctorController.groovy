ae5edf1b7531fbe08e407cf61f83e1f672eb6f4a
Zainny45 <b7015095@my.shu.ac.uk>
2019-04-07 15:45:06 +0100

    Created the receptionist login and created all the buttons

--- appointmentSystem/grails-app/controllers/com/zislam/DoctorController.groovy
+++ appointmentSystem/grails-app/controllers/com/zislam/DoctorController.groovy
@@ -3,4 +3,22 @@ package com.zislam
 class DoctorController {
 
     def scaffold= Doctor
+def login() {
+ }
+
+def validate() {
+def user = Doctor.findByDoctorEmail(params.email)
+if (user && user.password == params.password){
+session.user = user
+render view:'home'
+}
+else{
+flash.message = "Invalid email and password."
+render view:'login'
+}
+}
+def logout = {
+ session.user = null
+ redirect(uri:'/')
+ }
 }
